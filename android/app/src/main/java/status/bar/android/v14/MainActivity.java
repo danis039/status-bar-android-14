e79c05628df2f0ee51ef7d885ca32cbb7aca3785
Daniel Silva <daniel.silva@noaignite.com>
2024-07-16 12:17:50 +0200

    Setup Ionic Android blank project, install @capacitor/splash-screen and set StatusBar background color

---
+++ android/app/src/main/java/status/bar/android/v14/MainActivity.java
@@ -0,0 +1,5 @@
+package status.bar.android.v14;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "status.bar.android.v14",
+  appName: "status-bar-android-14",
+  webDir: "dist",
+};
+
+export default config;
